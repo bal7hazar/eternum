158382510a6349d8150749522f54c4cb476da037
bal7hazar <bal7hazar@proton.me>
2024-12-11 14:06:15 +0100

    🐛 Update timestamp

--- client/src/ui/components/scripts.tsx
+++ client/src/ui/components/scripts.tsx
@@ -10,7 +10,7 @@ import { Show } from "./scripts/show";
 import { Skip } from "./scripts/skip";
 import { Transfers } from "./scripts/transfers";
 
-const SEASON_START_TIMESTAMP = 1733921925;
+const SEASON_START_TIMESTAMP = 1733924970;
 
 export const Scripts = () => {
   const [unfolded, setUnfolded] = useState(true);
