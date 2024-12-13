3cffe85f16718a5274952de1aecd2c72f892d5d5
bal7hazar <bal7hazar@proton.me>
2024-12-13 14:07:53 +0100

    ✨ simplify transfers

--- client/src/ui/components/scripts/transfers.tsx
+++ client/src/ui/components/scripts/transfers.tsx
@@ -3,14 +3,13 @@ import useScriptStore, { Transfer } from "@/hooks/store/useScriptStore";
 import Button from "@/ui/elements/Button";
 import TextInput from "@/ui/elements/TextInput";
 import { multiplyByPrecision } from "@/ui/utils/utils";
-import { ResourcesIds, SendResourcesProps } from "@bibliothecadao/eternum";
+import { ResourcesIds } from "@bibliothecadao/eternum";
 import clsx from "clsx";
 import { Loader, Play } from "lucide-react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 export const Transfers = () => {
     const { transfers, setTransfers, resetTransfers } = useScriptStore();
-    const [isRunning, setIsRunning] = useState(false);
 
     const [hover, setHover] = useState(false);
     const onMouseEnter = useCallback(() => {
@@ -29,7 +28,19 @@ export const Transfers = () => {
   
     const [isLoading, setIsLoading] = useState(false);
   
-    const handleSend = async (calls: SendResourcesProps[]) => {
+    const handleSend = async () => {
+      if (!transfers || transfers.items.length === 0) return;
+      console.log(transfers);
+      const calls = transfers.items
+        .map(({ from, to, resource, amount }: Transfer) => {
+          const resourceId = ResourcesIds[resource as keyof typeof ResourcesIds];
+          return {
+            signer: account,
+            sender_entity_id: BigInt(from),
+            recipient_entity_id: BigInt(to),
+            resources: [BigInt(resourceId), BigInt(multiplyByPrecision(amount))],
+          }
+        });
       setIsLoading(true);
       try {
         await sends_resources(calls);
@@ -40,48 +51,13 @@ export const Transfers = () => {
       }
     };
 
-    useEffect(() => {
-      if (!isRunning) return;
-      // Get current timestamp
-      const times: { [key: number]: number } = {};
-      // Create an interval that runs every 1 second
-      const interval = setInterval(() => {
-        const timestamp = Math.floor(new Date().getTime() / 1000);
-        const calls = transfers.items
-          .filter(({ start, end, every }, index) => {
-            const condition = timestamp >= start && timestamp <= end && timestamp > (times[index] || 0);
-            const noset = start == undefined && end == undefined && every == undefined && times[index] == undefined;
-            if (condition || noset) {
-              times[index] = timestamp + every;
-            }
-            return condition || noset;
-          })
-          .map(({ from, to, resource, amount }: Transfer) => {
-            const resourceId = ResourcesIds[resource as keyof typeof ResourcesIds];
-            return {
-              signer: account,
-              sender_entity_id: BigInt(from),
-              recipient_entity_id: BigInt(to),
-              resources: [BigInt(resourceId), BigInt(multiplyByPrecision(amount))],
-            }
-          });
-        if (calls.length > 0) {
-          console.log(calls);
-          handleSend(calls);
-        }
-      }, 1000);
-
-      return () => clearInterval(interval);
-
-    }, [isRunning]);
-
     return (
       <div className="flex items-center gap-x-2">
         <h4 className="text-sm min-w-20">Transfers</h4>
         <TextInput onChange={(value: string) => value ? setTransfers(JSON.parse(value)) : resetTransfers()} placeholder="All" />
         <Button
           className={clsx("text-xs h-8", hover && "opacity-50")}
-          onClick={() => setIsRunning(true)}
+          onClick={handleSend}
           onMouseEnter={onMouseEnter}
           onMouseLeave={onMouseLeave}
         >
